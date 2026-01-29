cce2539cda357dc7acf6c5184331359577e8472f
John Donmoyer <john@deno.com>
2026-01-28 22:19:18 -0500

    add esbuild config to fix Deploy build

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [fresh()],
   build: {
     sourcemap: false,
+    minify: "esbuild",
+    target: "esnext",
+  },
+  esbuild: {
+    target: "esnext",
+    format: "esm",
   },
 });
