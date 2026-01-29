67231679ebdbe21bda709ee9330db097c95e19ef
John Donmoyer <jdonmoyer@gmail.com>
2026-01-29 13:02:13 -0500

    Updated _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>NBA Salary Model</title>
+        <title>NBA Salary Model Stats</title>
         <meta name="description" content="Calculate NBA contract value based on DARKO ratings and custom projections" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="stylesheet" href="/styles.css" />
