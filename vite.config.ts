ceb556438925e71f2e2e512424c93b8901c42ad9
John Donmoyer <john@deno.com>
2026-02-07 23:27:23 -0500

    allow tailscale hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { fresh } from "@fresh/plugin-vite";
 
 export default defineConfig({
   plugins: [fresh()],
+  server: {
+    allowedHosts: true,
+  },
   build: {
     sourcemap: false,
     minify: "esbuild",
